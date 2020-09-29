f1dc5c35526d1f1a80c17f03622b6e1739f094b4
victor <vic.sax.29@gmail.com>
2020-09-29 11:40:14 +0200

    Update main.java

--- laberinto/src/laberinto/main.java
+++ laberinto/src/laberinto/main.java
@@ -1,9 +1,27 @@
 package laberinto;
 
 public class main {
+	
+	int columna;
+	int fila;
+	Celda [][] laberinto;
+	Celda [] vecinos;
+	final String [] id_movimientos= {"N","E","S","O"};
+	int [][] movimientos= {(-1,0),(0,1),(1,0),(0,-1)};
+	
+	public void iniciar() {
+		int contador=0;
+		laberinto = new Celda [fila][columna];
+		for(int i=0; i<fila; i++) {
+			for(int j=0; j<columna; j++) {
+				contador++;
+				laberinto[i][j]=new Celda(contador, i, j);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
