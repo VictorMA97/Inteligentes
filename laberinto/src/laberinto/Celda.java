c8225f374f00827ddb3c14626665731f24e782a0
victor <vic.sax.29@gmail.com>
2020-09-29 10:45:04 +0200

    Update Celda.java

--- laberinto/src/laberinto/Celda.java
+++ laberinto/src/laberinto/Celda.java
@@ -1,5 +1,30 @@
 package laberinto;
 
 public class Celda {
+	
+	private int id;
+	private boolean visitado;
+	String [] vecinos=new String[4];
+	String [] id_movimientos= new String[4];
+	
+	public Celda() {
+		this.visitado=false;
+		this.id_movimientos= {'N','E','S','O'};
+	}
+	 
+	
+	public boolean isVisitado() {
+		return visitado;
+	}
+	public void setVisitado(boolean visitado) {
+		this.visitado = visitado;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 
 }
