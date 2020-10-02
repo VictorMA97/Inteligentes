d586e27c35fa7557afa1f8487f94e3429edf9f1d
victor <vic.sax.29@gmail.com>
2020-10-02 21:10:28 +0200

    Main perfeccionado
    Refinado de codigo

--- Inteligentes/src/Laberinto/Main.java
+++ Inteligentes/src/Laberinto/Main.java
@@ -1,14 +1,11 @@
 package laberinto;
 
 import java.util.Scanner;
-import laberinto.Gestor_Archivos.*;
-import laberinto.ContenidoFichero.*;
 
 public class Main {
 
     private static int fila, columna;
     private static Celda[][] laberinto;
-    private static final int max_vecinos = 4;
 
     public static void main(String[] args) {
         menu();
@@ -18,38 +15,34 @@ public class Main {
 
         Scanner teclado = new Scanner(System.in);
         boolean bucle = false;
-        String rut = "";
+        String rut;
+        rut = null;
         System.out.println("1.Generar laberinto.\n2.Leer fichero json.\n3.Salir.");
-        do {
-            System.out.println("Introduza la opcion del menu:");
-            try {
-                int option = teclado.nextInt();
-                switch (option) {
-                    case 1:
-                        pedir_datos();
-                        Wilson w = new Wilson();
-                        laberinto = w.getLaberinto();
-                        //Dibujamos
-                        break;
-                    case 2:
-                        rut = leer_json();
-                        Gestor_Archivos.leerArchivoJson(rut);//Leer json.
-                        break;
-                    case 3:
-                        System.exit(1);
-                        break;
-                    default:
-                        bucle = true;
-                        throw new Exception();
-                }
-            } catch (NumberFormatException e) {
-                System.err.println("Error solo datos numericos");
-            } catch (Exception e) {
-                e.getMessage();
-                //System.err.println("Error solo numeros del 1 al 3.");
+        System.out.println("Introduza la opcion del menu:");
+        try {
+            int option = teclado.nextInt();
+            switch (option) {
+                case 1:
+                    pedir_datos();
+                    Wilson w = new Wilson();
+                    laberinto = w.getLaberinto();
+                    //Dibujamos
+                    break;
+                case 2:
+                    rut = leer_json();
+                    Gestor_Archivos.leerArchivoJson(rut);//Leer json.
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.err.println("Solo valores entre 1 y 3.");
+                    menu();
             }
-
-        } while (bucle);
+        } catch (Exception e) {
+            System.err.println("Error solo datos numericos.");
+            menu();
+        }
     }
 
     private static void pedir_datos() {
