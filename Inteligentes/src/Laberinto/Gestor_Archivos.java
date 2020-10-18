62ba649846db02e3f39903f21af9621d187bc7ba
Rizoss <beatrizmv97@gmail.com>
2020-10-19 00:16:43 +0200

    Escritura del json
    Modificación en la parte de escritura del json. Utilizar json-20200518.jar

--- Inteligentes/src/Laberinto/Gestor_Archivos.java
+++ Inteligentes/src/Laberinto/Gestor_Archivos.java
@@ -7,6 +7,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import com.google.gson.*;
 import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Gestor_Archivos {
 
@@ -64,16 +66,68 @@ public class Gestor_Archivos {
 
     /*Escritura del json, para la escritura no necesito la celda en si, es mas cada celda del resultado*/
     public static void escribirArchivoJson(String ruta) {
-        Gson gson = new Gson(); //no s� si los parametros habr�a que pasarlos a string, o sea hacer la conversi�n
-        ruta += "\\Resultado.json";
-        Celda[][] laberinto = Main.getLaberinto();
-        try {
-        	FileWriter archivo = new FileWriter(ruta);
-        	archivo.write(gson.toJson(laberinto));
-        	archivo.flush();
-        	archivo.close();
-        }catch(IOException e) {
-        	System.out.println("Error al escribir json");
-        }
+    Gson gson = new Gson(); //no s� si los parametros habr�a que pasarlos a string, o sea hacer la conversi�n
+    ruta += "\\Resultado.json";
+    Main main = new Main();
+    JSONObject archivo = new JSONObject();
+    JSONObject cells = obtenerCells();
+    JSONArray id_movimiento = obtenerId();
+    archivo.put("row", main.getFila());
+    archivo.put("cols", main.getColumna());
+    archivo.put("max_n", 4); 
+    archivo.put("id_movimiento", id_movimiento);
+    archivo.put("cells", cells);
+    
+    try {
+    	FileWriter fichero2 = new FileWriter(ruta);
+    	fichero2.write(archivo.toString());
+    	fichero2.flush();
+    	fichero2.close();
+    	System.out.println("Fichero creado.");
+    }catch(IOException ex) {
+    	System.out.println("Error al escribir json");
     }
+    
+}
+public static JSONArray obtenerId() {
+	JSONArray movimiento = new JSONArray();
+    JSONArray lista = new JSONArray();
+    lista.put(-1);
+    lista.put(0);
+	movimiento.put(lista);
+	lista = remove(lista);
+	lista.put(0);
+	lista.put(1);
+	movimiento.put(lista);
+	lista = remove(lista);
+	lista.put(1);
+	lista.put(0);
+	movimiento.put(lista);
+	lista = remove(lista);
+	lista.put(0);
+	lista.put(-1);
+	movimiento.put(lista);
+    return movimiento;
+}
+public static JSONArray remove(JSONArray lista) {
+	for(int i=0; i< lista.length();i++) {
+		lista.remove(i);
+	}
+	return lista;
+}
+public static JSONObject obtenerCells() {
+	Main main = new Main();
+	Wilson wilson = new Wilson(main.getFila(), main.getColumna());
+	Celda[][] laberinto = wilson.getLaberinto();
+	JSONObject cells = new JSONObject();
+	for(int i=0; i<laberinto.length; i++) {
+		for(int j=0; j<laberinto.length; j++) {
+			JSONObject celda = new JSONObject();
+			boolean[] interiorCelda = laberinto[i][j].getVecinos();
+			celda.put("neighbors", interiorCelda);
+			cells.put("("+String.valueOf(i)+","+String.valueOf(j)+")",celda);
+		}
+	}
+	return cells;
+}
 }
