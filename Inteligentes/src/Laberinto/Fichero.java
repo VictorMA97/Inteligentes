83ef359224c1279e6a24387e2a7cac7e22d2debe
Beatriz Muñoz Vicioso <beatrizmv97@gmail.com>
2020-11-05 15:57:12 +0100

    Cambio en el formato
    He cambiado el método obtener celda para que cree el formato correcto a la hora de imprimir.

--- Inteligentes/src/Laberinto/Fichero.java
+++ Inteligentes/src/Laberinto/Fichero.java
@@ -8,44 +8,54 @@ public class Fichero {
 	private int row;
 	private int cols;
 	private int max_n;
-	private ArrayList id_movimiento;
-	private Map<String, Celda> cells;
+	private ArrayList mov;
+	private ArrayList id_mov;
+	private Map<String, ContenidoCelda> cells;
 	
 	public Fichero(Celda[][] laberinto){
 		this.row = laberinto.length;
 		this.cols = laberinto[0].length;
 		this.max_n = 4;
-		this.id_movimiento = obtenerId();
+		this.mov = obtenerId();
+		this.id_mov = obtenerMov();
 		this.cells = obtenerCelda(laberinto);
+	}
+	public ArrayList obtenerMov() {
+		ArrayList id_mov = new ArrayList();
+		id_mov.add("N");
+		id_mov.add("E");
+		id_mov.add("S");
+		id_mov.add("O");
+		return id_mov;	
 	}
 	
 	public ArrayList<ArrayList<Integer>> obtenerId() {
-		ArrayList<ArrayList<Integer>> id_movimiento = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> mov = new ArrayList<ArrayList<Integer>>();
 		ArrayList<Integer> a = new ArrayList<Integer>();
 		a.add(-1);
 		a.add(0);
-		id_movimiento.add(a);
+		mov.add(a);
 		ArrayList<Integer> b = new ArrayList<Integer>();
 		b.add(0);
 		b.add(1);
-		id_movimiento.add(b);
+		mov.add(b);
 		ArrayList<Integer> c = new ArrayList<Integer>();
 		c.add(1);
 		c.add(0);
-		id_movimiento.add(c);
+		mov.add(c);
 		ArrayList<Integer> d = new ArrayList<Integer>();
 		d.add(0);
 		d.add(-1);
-		id_movimiento.add(d);
-		return id_movimiento;
+		mov.add(d);
+		return mov;
 	}
-	public Map<String, Celda> obtenerCelda(Celda[][] lab){
-		Map<String, Celda> cel = new LinkedHashMap<String, Celda>();
-		//Map<String, String> fafa = new HashMap<String, String>();
+	public Map<String, ContenidoCelda> obtenerCelda(Celda[][] lab){
+		Map<String, ContenidoCelda> cel = new LinkedHashMap<String, ContenidoCelda>();
 		for(int i=0; i<lab.length; i++) {
 			for(int j=0; j<lab[0].length; j++) {
 				Celda celda = lab[i][j];
-				cel.put("("+String.valueOf(i)+","+String.valueOf(j)+")", celda);
+				ContenidoCelda cell = new ContenidoCelda(celda);
+				cel.put("("+String.valueOf(i)+","+String.valueOf(j)+")", cell);
 			}
 		}
 		return cel;
