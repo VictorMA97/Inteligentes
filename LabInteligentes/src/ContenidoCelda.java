36eb36bada57d89bfb619c8e15707afef59ef4ca
Beatriz Muñoz Vicioso <beatrizmv97@gmail.com>
2020-11-10 00:08:11 +0100

    Contenido celda

---
+++ LabInteligentes/src/ContenidoCelda.java
@@ -0,0 +1,13 @@
+package Pruebas;
+
+public class ContenidoCelda {
+	
+	private int value = 0;
+	private boolean[] vecinos;
+
+	ContenidoCelda(Celda celda){
+		this.vecinos = celda.getVecinos();
+		
+	}
+	
+}
