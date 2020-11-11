e7188cceeebb819d3d5341a209f3f713388a1a45
Beatriz Muñoz Vicioso <beatrizmv97@gmail.com>
2020-11-11 11:01:41 +0100

    Modificación de sucesores

--- LabInteligentes/src/Sucesores.java
+++ LabInteligentes/src/Sucesores.java
@@ -5,36 +5,16 @@ public class Sucesores {
     private String INITIAL;
     private String OBJETIVE;
     private String MAZE;
-    Celda inicio;
-    Celda fin;
-
-    public Celda getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(Celda inicio) {
-        this.inicio = inicio;
-    }
-
-    public Celda getFin() {
-        return fin;
-    }
-
-    public void setFin(Celda fin) {
-        this.fin = fin;
-    }
 
     public void nombreArchivo(String nombre) {
         this.MAZE = nombre;
     }
 
     public void sucesores(Celda inicio, Celda fin) {
-        Main nuevo = new Main();
-        //Gestor_Archivos gestor = new Gestor_Archivos();
-        int fila = inicio.getFila();
-        int columna = inicio.getColumna();
-        this.INITIAL = "(" + fila + ", " + columna + ")";
-        this.OBJETIVE = "(" + fila + ", " + columna + ")";
-        //this.MAZE = nombre;
+        this.INITIAL = "(" + inicio.getFila() + ", " + inicio.getColumna() + ")";
+        this.OBJETIVE = "(" + fin.getFila() + ", " + fin.getColumna() + ")";
     }
+    public String toString() {
+		return "{"+"INITIAL: " + INITIAL + ", OBJETIVE: " + OBJETIVE + ", MAZE: " + MAZE + "}";
+	}
 }
